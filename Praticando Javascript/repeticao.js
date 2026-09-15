8ed857d9abdbce655948104c9b5dfb2dba8cf7c2
LUÍS OTÁVIO COELHO FERREIRA <54150243875@info.local>
2026-09-15 14:28:48 -0300

    Terminado os niveis basicos

---
+++ Praticando Javascript/repeticao.js
@@ -0,0 +1,52 @@
+
+//CONTAGEM 1 A 10
+
+
+// let contagem = 1;
+
+// for (let contagem = 1; contagem <= 10; contagem++) {
+//     console.log(contagem);
+    
+// }
+
+
+
+
+
+//TABUADA
+
+
+// let numero = Number(prompt("Digite um número:" ))
+
+// for (let i= 1; i <= 10; i++) {
+//     console.log(numero + "x" + i + "=" + (numero * i));
+    
+    
+// }
+
+
+// SOMA DOS PRIMEIROS N NUMEROS NATURAIS
+
+// let numero =Number(prompt("Digite um numero N: "))
+
+// let soma =0
+
+
+//     for (let i = 1; i <numero; i++) {
+//        soma = soma + i
+        
+//     }
+//     console.log(soma);
+    
+
+
+//NUMEROS PARES DE 1 A 50
+
+let numero = 50 
+
+for ( i = 1; i<= numero; i +2) {
+    
+    console.log(i);
+    
+    
+}
---
+++ Praticando Javascript/script.js
@@ -0,0 +1,119 @@
+
+
+// let nome = "Diogo";
+// let idade = 30;
+
+// //  CONCATENÇÃO
+// console.log("Nome: " + nome + ", idade:" +idade);
+
+// // interposição
+// console.log(`Nome: ${nome}, idade: ${idade}`);
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+
+
+// let soma = "5" +3;
+// let sub = "5" -3;
+
+// console.log(soma);
+// console.log(sub);
+
+
+// console.log(typeof soma);
+// console.log(typeof sub);
+
+
+// let numero ="53"; //STRING
+
+// let convertido = Number(numero)   //Converte o valor para number
+
+// console.log(typeof convertido);
+
+
+//OPERADORES RELACIONAIS
+
+// //VALIDA IGUALDADE DE VALOR (SOMENTE VALOR)
+// "10" == 10 -> true
+
+
+// //VALIDO IGUALDADE DE VALOR E TIPO DE DADO
+// "10" === 10 -> false
+
+// // &&
+// // ||
+// // 
+
+
+// //ESTRUTURA DE REPETIÇÃO 
+// let frutas = ["Maça" ,"Banana", "Acerola", "Uva"];
+
+// console.log(frutas);
+
+// frutas.forEach(function(fruta) {
+//     console.log(`Fruta: ${fruta}`);
+// })
+
+// frutas.forEach(fruta => console.log(`Fruta: ${fruta}`)
+// )
+
+
+
+//FUNÇÕES NATIVAS JS
+//FUNÇÃO MAP()
+//PERCORRE UMA LISTA E CRIA UMA NOVA LISTA COM BASE EM UMA CONDIÇÃO 
+
+// let numeros =[1,2,3,4];
+
+// let numerosDobrados = numeros.map(function(num) {
+//     return num * 2
+// })
+
+//numerosDobrados = [2,4,6,8]
+
+// console.log(numerosDobrados);
+
+// ou assim
+//Arrow function com retorno implicito
+
+// let numerosDobradosArrow = numeros.map(num => num * 2);
+
+//FILTER
+// Filtra os elementos com base em uma condição 
+
+// let numeros = [5, 10, 15, 20];
+
+// let maioresQueDez = numeros.filter(num => num > 10);
+
+
+// //maioresQueDez = [15,20]
+// console.log(maioresQueDez);
+
+//REDUCE
+//Reduz os valores de um array para um único valor
+
+// let numeros = [1, 2, 3, 4];
+
+// //ACUMULADOR inicia em 0
+// //variavel auxiliar
+// //acumulador começa em 0 e soma com o auxiliar até o ultimo valor do array 
+
+// let soma = numeros.reduce((acumulador, auxiliar) => acumulador + auxiliar, 0);
+
+// console.log(soma);
+
+
+//FIND 
+//Retorna o primeiro elemento que atende a condição 
+
+// let produtos = [
+//     { id:1 , nome: "Teclado", preço: 50 } , //produtos
+//     { id:2 , nome: "Mouse", preço:30}
+    
+// ]
+
+// let item = produtos.find(produto => produto.id === 2)
+
+// console.log(item);
+
+
